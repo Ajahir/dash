80f5a9b30f9c8f36f3109e3de81a2b9839e2e1b1
aeris.ahir <aeris.ahir@1R-75>
2022-03-16 18:15:07 +0530

    Add project files.

---
+++ dash/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.BottomNavigation;
+using static Google.Android.Material.BottomNavigation.BottomNavigationView;
+namespace dash
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnNavigationItemSelectedListener
+    {
+        private View View;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.layout1);
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            View = FindViewById<View>(Resource.Id.view1);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+} 
